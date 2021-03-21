afcf00f351e3e328c7039078e1dd9fd14fb82b89
Lawrence O. Caulker <locaulker@gmail.com>
2021-03-21 11:07:36 -0400

    end of L144

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react"
+import Accordion from "./components/Accordion"
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a FrontEnd JavaScript Library",
+  },
+
+  {
+    title: "Why use React?",
+    content: "React is the favorite JS Library used by JS Developers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+]
 
 const App = () => {
-  return <div>Widgets App</div>
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
 }
 
 export default App
