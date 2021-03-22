cd0709e5df2a93db28c108b1e68710a6a4c853a1
Lawrence <locaulker@gmail.com>
2021-03-22 06:35:29 -0400

    start at L156 - useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react"
-import Accordion from "./components/Accordion"
+// import Accordion from "./components/Accordion"
+import Search from "./components/Search"
 
 const items = [
   {
     title: "What is React?",
-    content: "React is a FrontEnd JavaScript Library",
+    content: "React is a FrontEnd JavaScript Library"
   },
 
   {
     title: "Why use React?",
-    content: "React is the favorite JS Library used by JS Developers",
+    content: "React is the favorite JS Library used by JS Developers"
   },
   {
     title: "How do you use React?",
-    content: "You use React by creating components",
-  },
+    content: "You use React by creating components"
+  }
 ]
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   )
 }
