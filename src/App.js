aa078eddd139ad4e0d92f2021e2d62fcf481ec1f
Lawrence O. Caulker <locaulker@gmail.com>
2021-03-23 17:36:18 -0400

    end of L178. Added dropdown menu widget

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 // import Accordion from "./components/Accordion"
-import Search from "./components/Search"
+// import Search from "./components/Search"
+import Dropdown from "./components/Dropdown"
 
+// Accordion Widget Data
 const items = [
   {
     title: "What is React?",
-    content: "React is a FrontEnd JavaScript Library"
+    content: "React is a FrontEnd JavaScript Library",
   },
 
   {
     title: "Why use React?",
-    content: "React is the favorite JS Library used by JS Developers"
+    content: "React is the favorite JS Library used by JS Developers",
   },
   {
     title: "How do you use React?",
-    content: "You use React by creating components"
-  }
+    content: "You use React by creating components",
+  },
+]
+
+// Dropdown Widget Data
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ]
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0])
+
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   )
 }
