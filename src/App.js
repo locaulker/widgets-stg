025c16f65ea3221d331fd587e6903e3305d3f383
Lawrence <locaulker@gmail.com>
2021-03-25 06:40:38 -0400

    resume at L207

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search"
 import Dropdown from "./components/Dropdown"
 import Translate from "./components/Translate"
 import Route from "./components/Route"
+import Header from "./components/Header"
 
 // Accordion Widget Data
 const items = [
@@ -42,6 +43,7 @@ const App = () => {
   const [selected, setSelected] = useState(options[0])
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
